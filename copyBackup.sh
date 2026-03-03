cb5e657f15b2c86c5e7b62399df06b9be9b6746b
malklera <malklerulo@gmail.com>
2026-03-03 14:31:02 -0300

    feat: add XCompose to backup, make new repository filepath

--- copyBackup.sh
+++ copyBackup.sh
@@ -10,52 +10,58 @@ ORIGINAL_USER="$(whoami)"
 # Get the home directory of the original user
 HOME_DIR=$(eval echo "~$ORIGINAL_USER")
 
-# bash
-cp -u "$HOME_DIR/.bashrc" "$HOME_DIR/forArch/" || log_error "Failed to copy .bashrc."
+# Set up repository
+REPO="$HOME_DIR/forArch"
 
-cp -u "$HOME_DIR/.bash_profile" "$HOME_DIR/forArch/" || log_error "Failed to copy .bash_profile."
+# bash
+cp -u "$HOME_DIR/.bashrc" "$REPO/" || log_error "Failed to copy .bashrc."
+
+cp -u "$HOME_DIR/.bash_profile" "$REPO/" || log_error "Failed to copy .bash_profile."
 
 # neovim
-cp -ru "$HOME_DIR/.config/nvim/" "$HOME_DIR/forArch/.config/" || log_error "Failed to copy nvim."
+cp -ru "$HOME_DIR/.config/nvim/" "$REPO/.config/" || log_error "Failed to copy nvim."
 
 # tmux
-cp -u "$HOME_DIR/.tmux/tmux-close.sh" "$HOME_DIR/forArch/.tmux/" || log_error "Failed to copy tmux-close.sh."
-cp -u "$HOME_DIR/.tmux/tmux-start.sh" "$HOME_DIR/forArch/.tmux/" || log_error "Failed to copy tmux-start.sh."
-cp -u "$HOME_DIR/.config/tmux/tmux.conf" "$HOME_DIR/forArch/.config/tmux/" || log_error "Failed to copy tmux.conf."
+cp -u "$HOME_DIR/.tmux/tmux-close.sh" "$REPO/.tmux/" || log_error "Failed to copy tmux-close.sh."
+cp -u "$HOME_DIR/.tmux/tmux-start.sh" "$REPO/.tmux/" || log_error "Failed to copy tmux-start.sh."
+cp -u "$HOME_DIR/.config/tmux/tmux.conf" "$REPO/.config/tmux/" || log_error "Failed to copy tmux.conf."
 
 # custom layout
-cp -u "/usr/share/X11/xkb/symbols/custom" "$HOME_DIR/forArch/assets/keyboard/" || log_error "Failed to copy keyboard layout."
+cp -u "/usr/share/X11/xkb/symbols/custom" "$REPO/assets/keyboard/" || log_error "Failed to copy keyboard layout."
+
+# custom compose
+cp -u "$HOME_DIR/.XCompose" "$REPO/" || log_error "Failed to copy compose file."
 
 # ghostty
-cp -ru "$HOME_DIR/.config/ghostty" "$HOME_DIR/forArch/.config/" || log_error "Failed to copy ghostty."
+cp -ru "$HOME_DIR/.config/ghostty" "$REPO/.config/" || log_error "Failed to copy ghostty."
 
 # btop
-cp -ru "$HOME_DIR/.config/btop/" "$HOME_DIR/forArch/.config/" || log_error "Failed to copy btop."
+cp -ru "$HOME_DIR/.config/btop/" "$REPO/.config/" || log_error "Failed to copy btop."
 
 # hyprland
-cp -ru "$HOME_DIR/.config/hypr/" "$HOME_DIR/forArch/.config/" || log_error "Failed to copy hypr."
+cp -ru "$HOME_DIR/.config/hypr/" "$REPO/.config/" || log_error "Failed to copy hypr."
 
 # dunst
-cp -ru "$HOME_DIR/.config/dunst/" "$HOME_DIR/forArch/.config/" || log_error "Failed to copy dunst."
+cp -ru "$HOME_DIR/.config/dunst/" "$REPO/.config/" || log_error "Failed to copy dunst."
 
 # waybar
-cp -ru "$HOME_DIR/.config/waybar/" "$HOME_DIR/forArch/.config/" || log_error "Failed to copy waybar."
+cp -ru "$HOME_DIR/.config/waybar/" "$REPO/.config/" || log_error "Failed to copy waybar."
 
 # wlogout
-cp -ru "$HOME_DIR/.config/wlogout/" "$HOME_DIR/forArch/.config/" || log_error "Failed to copy wlogout."
+cp -ru "$HOME_DIR/.config/wlogout/" "$REPO/.config/" || log_error "Failed to copy wlogout."
 
 # mimeapps
-cp -u "$HOME_DIR/.config/mimeapps.list" "$HOME_DIR/forArch/.config/" || log_error "Failed to copy mimeapps."
+cp -u "$HOME_DIR/.config/mimeapps.list" "$REPO/.config/" || log_error "Failed to copy mimeapps."
 
 # user-dirs
-cp -u "$HOME_DIR/.config/user-dirs.dirs" "$HOME_DIR/forArch/.config/" || log_error "Failed to copy user-dirs.dirs."
-cp -u "$HOME_DIR/.config/user-dirs.locale" "$HOME_DIR/forArch/.config/" || log_error "Failed to copy user-dirs.locale."
+cp -u "$HOME_DIR/.config/user-dirs.dirs" "$REPO/.config/" || log_error "Failed to copy user-dirs.dirs."
+cp -u "$HOME_DIR/.config/user-dirs.locale" "$REPO/.config/" || log_error "Failed to copy user-dirs.locale."
 
 # thunar
-cp -u "$HOME_DIR/.config/Thunar/uca.xml" "$HOME_DIR/forArch/.config/Thunar/" || log_error "Failed to copy uca.xml."
+cp -u "$HOME_DIR/.config/Thunar/uca.xml" "$REPO/.config/Thunar/" || log_error "Failed to copy uca.xml."
 
 # desktop files
-cp -u "$HOME_DIR/.local/share/applications/"*.desktop "$HOME_DIR/forArch/.local/share/applications/" || log_error "Failed to copy applications files."
+cp -u "$HOME_DIR/.local/share/applications/"*.desktop "$REPO/.local/share/applications/" || log_error "Failed to copy applications files."
 
 # rofi
-cp -ru "$HOME_DIR/.config/rofi/" "$HOME_DIR/forArch/.config/" || log_error "Failed to copy rofi."
+cp -ru "$HOME_DIR/.config/rofi/" "$REPO/.config/" || log_error "Failed to copy rofi."
